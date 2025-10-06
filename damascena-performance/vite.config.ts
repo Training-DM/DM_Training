011a87f048672ba57ccefb62219bcd3df899470f
Training-DM <dmtraining20.3@gmail.com>
2025-10-06 14:11:14 -0300

    Update vite.config.ts

--- damascena-performance/vite.config.ts
+++ damascena-performance/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
     },
     plugins: [react()],
-    base: "/DM_Training",
+    base: "/DM_Training/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
